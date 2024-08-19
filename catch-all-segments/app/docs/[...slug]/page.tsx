e68468335d44cf2155b42b18640271d01c882167
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-08-20 03:20:31 +0500

    Routing Concept

--- catch-all-segments/app/docs/[...slug]/page.tsx
+++ catch-all-segments/app/docs/[...slug]/page.tsx
@@ -1,1 +1,20 @@
-
+export default function Docs({
+    params,
+}: {
+    params:{
+        slug : string[]
+    }
+}){
+    if(params.slug.length === 2){
+        return (
+            <h1>
+                Viewing documents for feature {params.slug[0]} and concept {params.slug[1]}
+            </h1>
+        );
+    }
+    else if(params.slug.length === 1){
+        return <h1>Viewing for documents for feature {params.slug[0]}</h1>
+     
+    }
+    return <h1>Documents Home Page!</h1>
+}
