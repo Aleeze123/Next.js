8e632fe9b050aea9bbdb9e8acc854fe88c3adf5c
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-08-19 20:16:34 +0500

    Add files via upload

---
+++ nested-routes/src/app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+export default function FirstBlog(){
+    return(
+        <h1>My First Blog post!</h1>
+    )
+}
---
+++ nested-routes/src/app/blog/page.tsx
@@ -0,0 +1,5 @@
+export default function Blog(){
+    return(
+        <h1>My Blogs</h1>
+    )
+}
