3f88b0ff81fe84508a026dba9ea9846e93a3c853
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-08-19 20:28:33 +0500

    Add files via upload

---
+++ dynamic-routes/src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Productlist(){
+    return(
+        <>
+        <h1>Product Lists</h1>
+        <h2>Product 1</h2>
+        <h2>Product 2</h2>
+        <h2>Product 3</h2>        
+        </>
+    );
+}
