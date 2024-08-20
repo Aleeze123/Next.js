5e4c9e453ef12c9fcb53b78368c3534c7534a3d7
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-08-21 01:53:49 +0500

    Delete Loading & Error Handling/src/app/about/loading.tsx

--- Loading & Error Handling/src/app/about/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <h2 className="text-xl font-semibold text-gray-700">Please wait for a moment!</h2>
-        </div>
-    );
-}
