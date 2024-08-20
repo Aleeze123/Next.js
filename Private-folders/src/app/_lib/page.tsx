a23c8e5095e11354c222cfde50696065e8290a1b
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-08-20 05:04:41 +0500

    Add files via upload

---
+++ Private-folders/src/app/_lib/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateRoute(){
+    return <h1>We can't view this in the browser because the file is located in a private folder. </h1>
+}
---
+++ Private-folders/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home(){
+  return(
+    <h1>Welcome to home page!</h1>
+  )
+}
